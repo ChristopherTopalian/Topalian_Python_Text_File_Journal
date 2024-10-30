01716f7c13e97241480903a6da236d1e9f9adb92
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-30 16:18:18 -0400

    Delete Topalian_Python_Text_File_Journal directory

--- Topalian_Python_Text_File_Journal/pdf/combine/Topalian_Combine_txt_Files_One_Folder.bat
+++
@@ -1,9 +0,0 @@
-:: Dedicated to God the Father
-:: All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
-:: https://github.com/ChristopherTopalian
-:: https://github.com/ChristopherAndrewTopalian
-:: Topalian_Combine_txt_Files_One_Folder.bat
-
-@echo off
-copy *.txt main.txt
-
